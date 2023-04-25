5846e096af5d0e80e1009d0aab1566968905e575
StephansWelt <118051755+StephansWelt@users.noreply.github.com>
2023-04-25 11:21:23 +0200

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,4 +1,3 @@
-#Anmelde und Register Formular von EinfxchPingu
 import pymongo
 import time
 import os
@@ -7,7 +6,7 @@ from simple_colors import *
 #definiere verbindung zum Server | Abfrage für Anmelden oder registrieren
 def verbinde_server():
     print(red("Verbinde zum Server..."))
-    client = pymongo.MongoClient("mongodb+srv://<username>:<password>@<Atlas Clusters>.k3jmp5m.mongodb.net/test?retryWrites=true&w=majority")
+    client = pymongo.MongoClient("mongodb+srv://PythonAF:PythonAF@<Atlas Clusters>.k3jmp5m.mongodb.net/test?retryWrites=true&w=majority")
     db = client["meine_db"]
     benutzer_collection = db["benutzer"]
     time.sleep(3)
@@ -22,22 +21,38 @@ def verbinde_server():
 
 #Definiere anmelde Forumlar
 def anmelden(benutzer_collection):
-    benutzername = input(blue("Gib deinen Benutzernamen ein: "))
-    passwort = input(blue("Gib dein Passwort ein: "))
+    benutzername = input(green("Gib deinen Benutzernamen ein: "))
+    passwort = input(green("Gib dein Passwort ein: "))
     benutzer = benutzer_collection.find_one({"benutzername": benutzername, "passwort": passwort})
     if benutzer:
         print(blue(f"Herzlich Willkommen zurück {benutzername}"))
+        Hauptmenü(benutzername, benutzer_collection)
     else:
         print(red("Die Email oder das Password ist Falsch."))
+        print(blue("Bitte widerhole die Anmeldung","bold"))
+        print(blue("--------------------------"))
+        anmelden(benutzer_collection)
 
 #Definiere registrierungs Formular
 def registrieren(benutzer_collection):
-    benutzername = input(blue("Gib einen Benutzernamen ein: "))
-    passwort = input(blue("Gib ein Passwort ein: "))
+    benutzername = input(green("Gib einen Benutzernamen ein: "))
+    passwort = input(green("Gib ein Passwort ein: "))
     benutzer = {"benutzername": benutzername, "passwort": passwort}
     benutzer_collection.insert_one(benutzer)
     print(blue(f"Herzlich Willkommen {benutzername}!"))
+    Hauptmenü(benutzername, benutzer_collection)
+
+def Hauptmenü(benutzername, benutzer_collection):
+    print()
+    print(blue("HAUPTMENÜ"))
+    print(black("------------"))
+    print(green("1) Konto löschen"))
+    Eingabe12 = input()
+    if Eingabe12 == "1":
+        myquery = { "benutzername": benutzername }
+        benutzer_collection.delete_one(myquery)
+        print(blue(f"{benutzername} wurde gelöscht."))
+        verbinde_server()
 
 verbinde_server()
 
-#Ersetzte <username> , <password> und <Atlas Clusters> durch deine Daten von MongoDB.
